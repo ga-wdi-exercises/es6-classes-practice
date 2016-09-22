5d0f2a10211f67f1f02fe525eb8c00a391ef0937
Adrian Maseda <amaseda@gmail.com>
2016-09-22 09:22:47 -0400

    add soln w bonus

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 class Atm {
-  constructor(type){
+  constructor(type, backup=null){
     this.type = type;
     this.money = 0;
     this.transactionHistory = [];
+    this.backupAccount = backup;
   }
   withdraw(amount){
     this.money -= amount;
-    this.transactionHistory.push(-amount)
+    this.transactionHistory.push(-amount);
+    if(this.money < 0){
+      console.log(`Dipping into ${this.backupAccount.type}!`);
+      let difference = -this.money;
+      this.money = 0;
+      this.backupAccount.withdraw(difference);
+    }
   }
   deposit(amount){
     this.money += amount;
-    this.transactionHistory.push(amount)
+    this.transactionHistory.push(amount);
   }
   showBalance(){
     console.log("The current balance is:", this.money);
@@ -18,3 +25,4 @@ class Atm {
 }
 
 let savings = new Atm("savings");
+let checking = new Atm("checking", savings);
